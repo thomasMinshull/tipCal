0c14b388d8861a36a3f77de3ffafeabfa14c493e
Thomas Minshull <thomasminshull@gmail.com>
2016-07-08 15:06:10 -0700

    fin

--- TipCal/ViewController.m
+++ TipCal/ViewController.m
@@ -9,12 +9,13 @@
 #import "ViewController.h"
 
 #define INITIAL_LABEL_TEXT @"Enter your Bill amount to Calculate tip"
-#define LABEL_PREFIX @"Your Total bill, inlcuding tip comes to: "
+#define TOTAL_LABEL_PREFIX @"Your Total bill, inlcuding tip comes to: "
 #define TIP_LABEL_PREFIX @"Tip Percentage: "
 
 @interface ViewController ()
 @property (assign, nonatomic, readonly) float tipAmount;
 @property (assign, nonatomic) float tipPercentage;
+@property (assign, nonatomic) float billAmount;
 @property (weak, nonatomic) IBOutlet UILabel *tipPercentLabel;
 @property (weak, nonatomic) IBOutlet UILabel *totalLabel;
 @property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
@@ -34,6 +35,7 @@
 }
 
 - (void)viewWillAppear:(BOOL)animated {
+    self.billAmount = 0.0;
     self.tipPercentage = 0.15;
     self.slider.value = self.tipPercentage;
     [self adjustTipPercentage:nil]; // sets label
@@ -57,7 +59,7 @@
 
 // Just for practice make a calculated property
 - (float)tipAmount {
-    return [self.billAmountTextField.text floatValue] * (1 + self.tipPercentage);
+    return self.billAmount * (1 + self.tipPercentage);
 }
 
 #pragma mark -Actions 
@@ -67,19 +69,33 @@
     [self keyBoardWillHide:nil];
 }
 
-- (IBAction)CalculateTip:(id)sender {
-    if (self.totalLabel == 0) {
-        [self.totalLabel setText:INITIAL_LABEL_TEXT];
-    } else {
-        [self.totalLabel setText:[NSString stringWithFormat:@"%@%.2f", LABEL_PREFIX, self.tipAmount]];
-    }
-   
-}
-
 - (IBAction)adjustTipPercentage:(UISlider *)sender {
     self.tipPercentage = self.slider.value;
+    [self updateLabels];
+}
+
+- (IBAction)billAmountChanged:(UITextField *)sender {
+    self.billAmount = [sender.text doubleValue];
+    [self updateLabels];
+}
+
+- (IBAction)buttonTapped:(UIButton *)sender {
+    [sender setTitle:@"This button doesn't do anything!" forState:UIControlStateNormal];
+}
+
+
+#pragma mark -Helper Methods
+
+- (void)updateLabels {
+    if (self.billAmount == 0.0) {
+        [self.totalLabel setText:INITIAL_LABEL_TEXT];
+    } else {
+        [self.totalLabel setText:[NSString stringWithFormat:@"%@%.2f", TOTAL_LABEL_PREFIX, self.tipAmount]];
+    }
     [self.tipPercentLabel setText:[NSString stringWithFormat:@"%@%.2f", TIP_LABEL_PREFIX, self.tipPercentage]];
 }
+
+
 #pragma mark -KeyBoard Handling
 
 - (void)keyBoardDidShow:(NSNotification *)paramNotification {
